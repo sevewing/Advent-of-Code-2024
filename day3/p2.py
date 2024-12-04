7798e1d9229a60b70c6ce6119090182c71c7820a
Sevewing <liweisi8121@hotmail.com>
2024-12-04 21:05:41 +0100

    clean

--- day3/p2.py
+++ day3/p2.py
@@ -25,29 +25,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-# import re
-
-# with open('./day3/input.txt') as file:
-#     text = file.read()
-
-# lines_to_parse = []
-
-# to_check = text.split("do()")
-# for item in to_check:
-#     lines_to_parse.append(item.split("don't()")[0])
-
-
-# total = 0
-# for line in lines_to_parse:
-#     matches = re.findall(r'mul\(\d{1,3},\d{1,3}\)', line)
-#     for match in matches:
-#         left, right = match.split(",")
-#         left = int(left.split("(")[1])
-#         right = int(right.split(")")[0])
-#         total += left * right
-
-# print(total)
